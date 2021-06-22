1a8139b6a026eefd51eef39883d0e93570a45871
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-22 21:57:34 +0600

    Hello World print

---
+++ hello.js
@@ -0,0 +1,8 @@
+// Print for all text, number, float, string
+
+console.log('Hello World!');
+console.log('I Love Bangladesh');
+console.log(50);
+console.log(50.458);
+
+console.log("My Number : " + 66);
