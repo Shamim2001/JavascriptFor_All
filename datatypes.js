5258e481d8eb4d23f1ea7246feacb3fdcc559fb1
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-23 00:03:49 +0600

    Number, float num convert

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+//   ****** Number *******
+
+// srting to number conver
+
+var n = '100.876';
+
+convNum = Number(n);
+
+// Number convert
+console.log(convNum);
+
+
+// Float Number conver
+console.log(Number.parseFloat(convNum))
+
+// int num conver
+console.log(Number.parseInt(convNum))
+
+
+// max value store
+console.log(Number.MAX_VALUE);
+
+// min value store
+console.log(Number.MIN_SAFE_INTEGER);
