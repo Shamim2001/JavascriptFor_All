ea6d62cf789558b0c81d4f731096e77b0be2801a
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-23 12:31:22 +0600

    check value

--- typeconversion.js
+++ typeconversion.js
@@ -1,28 +1,28 @@
-var str = '5000';
-var n = 10;
+// var str = '5000';
+// var n = 10;
 
 
-// console.log(str * n)
-console.log(Number.parseInt(str))
-console.log(n.toString(n))
+// // console.log(str * n)
+// console.log(Number.parseInt(str))
+// console.log(n.toString(n))
 
-// falsy values
-''
-0
-null
-undefined
-NaN
+// // falsy values
+// ''
+// 0
+// null
+// undefined
+// NaN
 
-console.log(Boolean(''))
-console.log(Boolean('Shamim ahmed'))
+// console.log(Boolean(''))
+// console.log(Boolean('Shamim ahmed'))
 
-console.log(Boolean(null))
-console.log(Boolean(undefined))
+// console.log(Boolean(null))
+// console.log(Boolean(undefined))
 
 
-console.log(Boolean(0))
-console.log(Boolean(123))
+// console.log(Boolean(0))
+// console.log(Boolean(123))
 
-// Boolean to Srting conver
-var x = true;
-console.log(x.toString())
+// // Boolean to Srting conver
+// var x = true;
+// console.log(x.toString())
