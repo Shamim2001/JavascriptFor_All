4f2cb65aa0e73db7f72589b94aac35f9bf3681cf
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-24 12:50:16 +0600

    All operator Practise

---
+++ oparetor.js
@@ -0,0 +1,62 @@
+//  Arithmetic Operator
+//  + - * % ++ -- 
+
+// Remainder %
+// var a = 10
+// var b = 20
+
+// // console.log(a % b);
+
+// // console.log(a++)
+// // console.log(a)
+
+
+// // console.log(--a)
+// // console.log(a)
+
+
+// // assinment oparetor
+// a -= b
+// console.log(a)
+
+
+// a += b
+// console.log(a)
+
+
+// a *= b
+// console.log(a)
+
+
+// a /= b
+// console.log(a)
+
+
+// a %= b 
+// console.log(a)
+
+
+// Comparison Operator 
+
+var a = 10 
+var b = 20 
+
+console.log(a == b)
+console.log(a != b)
+
+console.log(a > b)
+console.log(a < b)
+
+
+// Logical Oparetor 
+// &&
+// ||
+// !
+
+// Bitwise Operator 
+// &
+// |
+// ~
+// ^
+// <<
+// >>
