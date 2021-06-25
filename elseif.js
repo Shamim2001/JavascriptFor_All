3174b9698bae25baf0a4dd53f44221b79a5c28eb
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-25 11:09:57 +0600

    elseif condition check

---
+++ elseif.js
@@ -0,0 +1,24 @@
+var a = 9
+var b = 10
+
+if (a > b) {
+    console.log("a is greater than b")
+} else if (a < b) {
+    console.log(" B is greater than A")
+} else {
+    console.log('They Both are same')
+}
+
+
+
+
+
+var n = 5
+
+if (n === 0){
+    console.log(n + ' is Zero')
+} else if (n % 2 === 0) {
+    console.log(n + " is Even number")
+} else {
+    console.log(n + " is Odd Number")
+}
