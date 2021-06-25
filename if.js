a155415b45aa17f6eb1088611b9a9aef41a5415f
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-25 10:51:06 +0600

    if condition check

---
+++ if.js
@@ -0,0 +1,20 @@
+var a = 10
+var b = 2
+
+if (a > b) {
+    console.log("a is greater than b")
+}
+
+if (a < b) {
+    console.log(" B is greater than A")
+}
+
+var n = 52
+
+if (n % 2 === 0) {
+    console.log(n + " is even number")
+}
+
+if (n % 2 === 1) {
+    console.log(n + " is ODD Number")
+}
