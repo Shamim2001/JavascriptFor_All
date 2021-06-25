5caac170438a217b0a190d556515c4ac2ca17568
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-25 10:57:08 +0600

    else condition add

---
+++ else.js
@@ -0,0 +1,16 @@
+var a = 10
+var b = 2
+
+if (a > b) {
+    console.log("a is greater than b")
+} else{
+    console.log(" B is greater than A")
+}
+
+var n = 51
+
+if (n % 2 === 0) {
+    console.log(n + " is Even number")
+} else {
+    console.log(n + " is Odd Number")
+}
