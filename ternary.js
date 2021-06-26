2b369521669368a3dce8ac6a30aaf67b1f01c40b
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-26 11:00:36 +0600

    ternary Operator

---
+++ ternary.js
@@ -0,0 +1,19 @@
+var n = 11
+var str = ''
+
+// if else condition , even or odd  number check
+
+if (n %2 === 0) {
+    str = "Even"
+} else {
+    str = "Odd"
+}
+
+console.log(str)
+
+
+// ternary operator even or odd number check
+
+var result = n % 2 === 0 ? 'EVEN' : 'ODD'
+
+console.log(result)
