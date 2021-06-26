d2682dfc399c9e8805eef2c43a1aaee805ace37e
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-26 12:42:13 +0600

    for loop

---
+++ forloop.js
@@ -0,0 +1,27 @@
+
+// for (var i = 0; i < 1000; i++) {
+//     console.log((i+1) + " Shamim Ahmed")
+// }
+
+// 1 to 10 plus(+) result
+
+// var sum = 0
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(sum + ' + ' + i + ' = ' + (sum + i))
+//     sum += i
+// }
+// console.log('result = ' + sum)
+
+
+
+var sum = 0
+
+
+
+for (var i = 1; i <= 10; i++) {
+
+    console.log(sum + ' + ' + i + ' = ' + (sum + i))
+    sum += i
+}
+console.log('result = ' + sum)
