2900bd9d91938e3a9624e3e1be0a5826bc698362
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-28 10:49:26 +0600

    add for loop, do while loop

---
+++ chapter_four/dowhile.js
@@ -0,0 +1,9 @@
+var present = false;
+
+while (present) {
+    console.log('Codding try While')
+}
+
+do {
+    console.log("present try do while loop!")
+} while (present)
--- chapter_four/while.js
+++ chapter_four/while.js
@@ -2,5 +2,11 @@ var present = true
 
 while(present) {
     var rand = Math.floor(Math.random() * 10 + 1)
-    console.log(rand)
+    if (rand === 9) {
+        console.log('I am Winner ')
+        present = false
+    } else {
+        console.log('Not winner')
+    }
 }
+
