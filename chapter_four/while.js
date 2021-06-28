c293f4c3f026b93ccedebeaf8ace14a9e70f7c46
shamim2001 <shamimahmedridoy2016@gamil.com>
2021-06-28 12:08:24 +0600

    break statement

--- chapter_four/while.js
+++ chapter_four/while.js
@@ -4,7 +4,7 @@ while(present) {
     var rand = Math.floor(Math.random() * 10 + 1)
     if (rand === 9) {
         console.log('I am Winner ')
-        present = false
+        break
     } else {
         console.log('Not winner')
     }
